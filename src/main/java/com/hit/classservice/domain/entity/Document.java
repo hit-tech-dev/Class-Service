58ccd1b7e2d3daf87fc21fdabbe9069e31e38140
Thang Nguyen Van <thang.nv2882@gmail.com>
2022-10-07 09:25:52 +0700

    [spint-1][code] create table document and table subject_schedule_relation

---
+++ src/main/java/com/hit/classservice/domain/entity/Document.java
@@ -0,0 +1,25 @@
+package com.hit.classservice.domain.entity;
+
+import com.hit.classservice.domain.entity.base.AbstractAuditingEntity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Document extends AbstractAuditingEntity {
+
+    private Long id;
+
+    private String link;
+
+    private Integer type;
+
+    private String title;
+
+    private double mark;
+
+}
