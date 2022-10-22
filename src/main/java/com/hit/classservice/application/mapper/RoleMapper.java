f2ddf66da9b9ac0153af83bbc9f4a902ec5ad3fb
Lam Linh <84983990+dieplinh0510@users.noreply.github.com>
2022-10-22 14:51:28 +0700

    [sprint-3][code]api get all role (#32)

--- src/main/java/com/hit/classservice/application/mapper/RoleMapper.java
+++ src/main/java/com/hit/classservice/application/mapper/RoleMapper.java
@@ -3,12 +3,16 @@ package com.hit.classservice.application.mapper;
 import com.hit.classservice.adapter.web.v1.transfer.parameter.role.UpdateRoleParameter;
 import com.hit.classservice.application.input.role.UpdateRoleInput;
 import com.hit.classservice.application.output.category.GetCategoryOutput;
+import com.hit.classservice.application.output.category.GetListCategoryItemOutput;
+import com.hit.classservice.application.output.role.GetListRoleItemOutput;
 import com.hit.classservice.application.output.role.GetRoleOutput;
 import com.hit.classservice.domain.entity.Category;
 import com.hit.classservice.domain.entity.Role;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Mappings;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface RoleMapper {
@@ -27,4 +31,7 @@ public interface RoleMapper {
   })
   GetRoleOutput toGetRoleOutput(Role role);
 
+  GetListRoleItemOutput toGetListRoleItemOutput(Role role);
+
+  List<GetListRoleItemOutput> toGetListRoleItemOutputs(List<Role> roles);
 }
