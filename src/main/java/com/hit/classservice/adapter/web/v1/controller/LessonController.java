88556b143c834168c563103c11f026c5ec54b5af
dochien0204 <doxuanchienh02042002@gmail.com>
2022-10-29 10:57:20 +0700

    [sprint-4][code] write docs api lesson

--- src/main/java/com/hit/classservice/adapter/web/v1/controller/LessonController.java
+++ src/main/java/com/hit/classservice/adapter/web/v1/controller/LessonController.java
@@ -1,15 +1,26 @@
 package com.hit.classservice.adapter.web.v1.controller;
 
 import com.hit.classservice.adapter.web.base.RestApiV1;
+import com.hit.classservice.adapter.web.base.RestData;
 import com.hit.classservice.adapter.web.base.VsResponseUtil;
 import com.hit.classservice.adapter.web.v1.transfer.parameter.lesson.CreateLessonParameter;
 import com.hit.classservice.adapter.web.v1.transfer.parameter.lesson.UpdateLessonParameter;
 import com.hit.classservice.adapter.web.v1.transfer.response.ResponseHeader;
 import com.hit.classservice.application.UseCaseBus;
+import com.hit.classservice.application.constant.CommonConstant;
 import com.hit.classservice.application.constant.UrlConstant;
 import com.hit.classservice.application.input.lesson.*;
 import com.hit.classservice.application.mapper.LessonMapper;
+import com.hit.classservice.application.output.category.CreateCategoryOutput;
+import com.hit.classservice.application.output.category.DeleteCategoryOutput;
+import com.hit.classservice.application.output.category.GetCategoryOutput;
+import com.hit.classservice.application.output.category.UpdateCategoryOutput;
 import com.hit.classservice.application.output.lesson.*;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.mapstruct.factory.Mappers;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.ResponseEntity;
@@ -30,6 +41,23 @@ public class LessonController {
     this.lessonMapper = Mappers.getMapper(LessonMapper.class);
   }
 
+  /**
+   *
+   * @param subjectId
+   * @return
+   * @throws Exception
+   */
+  @Operation(summary = "API get list lesson by subject id")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "Found the lesson",
+          content = {@Content(mediaType = CommonConstant.APPLICATION_JSON_TYPE,
+              schema = @Schema(implementation = GetListLessonBySubjectIdOutput.class))
+          }),
+      @ApiResponse(responseCode = "500", description = "Subject Id not exist", content = {
+          @Content(mediaType = CommonConstant.APPLICATION_JSON_TYPE,
+              schema = @Schema(implementation = RestData.class))
+      })
+  })
   @GetMapping(UrlConstant.Lesson.GET_LESSON_BY_SUBJECT)
   public ResponseEntity<?> getListLessonBySubjectId(@PathVariable(name = "subjectId") Long subjectId) throws Exception {
     // Create input
@@ -40,6 +68,23 @@ public class LessonController {
     return VsResponseUtil.ok(this.responseHeader.getHeader(), output);
   }
 
+  /**
+   *
+   * @param parameter
+   * @return
+   * @throws Exception
+   */
+  @Operation(summary = "API update lesson")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "Updated successfully", content = {
+          @Content(mediaType = CommonConstant.APPLICATION_JSON_TYPE,
+              schema = @Schema(implementation = UpdateLessonOutput.class))
+      }),
+      @ApiResponse(responseCode = "500", description = "Updated failed, some fields has errors", content = {
+          @Content(mediaType = CommonConstant.APPLICATION_JSON_TYPE,
+              schema = @Schema(implementation = RestData.class))
+      })
+  })
   @PutMapping(UrlConstant.Lesson.UPDATE)
   public ResponseEntity<?> updateLesson(@Valid @RequestBody UpdateLessonParameter parameter) throws Exception {
     // Create input
@@ -50,6 +95,19 @@ public class LessonController {
     return VsResponseUtil.ok(this.responseHeader.getHeader(), output);
   }
 
+  /**
+   *
+   * @param parameter
+   * @return
+   * @throws Exception
+   */
+  @Operation(summary = "API create new lesson")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "Created successfully",
+          content = {@Content(mediaType = CommonConstant.APPLICATION_JSON_TYPE,
+              schema = @Schema(implementation = CreateLessonOutput.class))
+          })
+  })
   @PostMapping(UrlConstant.Lesson.CREATE)
   public ResponseEntity<?> createSubject(@Valid @RequestBody CreateLessonParameter parameter) throws Exception {
     //Create input
@@ -66,6 +124,17 @@ public class LessonController {
    * @param subjectId Long
    * @return ResponseEntity<?>
    */
+  @Operation(summary = "API get list lessons detail by subject Id")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "Found the list lessons detail",
+          content = {@Content(mediaType = CommonConstant.APPLICATION_JSON_TYPE,
+              schema = @Schema(implementation = GetListLessonDetailOutput.class))
+          }),
+      @ApiResponse(responseCode = "500", description = "Subject Id not exist", content = {
+          @Content(mediaType = CommonConstant.APPLICATION_JSON_TYPE,
+              schema = @Schema(implementation = RestData.class))
+      })
+  })
   @GetMapping(UrlConstant.Lesson.LIST_LESSON_DETAIL)
   public ResponseEntity<?> getListLessonDetailBySubjectId(@PathVariable(name = "subjectId") Long subjectId) throws Exception {
     //Create input
@@ -76,6 +145,23 @@ public class LessonController {
     return VsResponseUtil.ok(this.responseHeader.getHeader(), output);
   }
 
+  /**
+   *
+   * @param id
+   * @return
+   * @throws Exception
+   */
+  @Operation(summary = "API delete lesson by id")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "Deleted successfully", content = {
+          @Content(mediaType = CommonConstant.APPLICATION_JSON_TYPE,
+              schema = @Schema(implementation = DeleteLessonOutput.class))
+      }),
+      @ApiResponse(responseCode = "500", description = "Id not exist", content = {
+          @Content(mediaType = CommonConstant.APPLICATION_JSON_TYPE,
+              schema = @Schema(implementation = RestData.class))
+      })
+  })
   @DeleteMapping(UrlConstant.Lesson.DELETE)
   public ResponseEntity<?> deleteLessonById(@PathVariable("id") Long id) throws Exception {
     // Create input
