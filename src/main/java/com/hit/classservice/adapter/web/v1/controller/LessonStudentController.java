495a21244862278247d05e3e007b9218fb65cb8f
Nguyễn Văn Việt <vanvietgg183@gmail.com>
2022-10-29 11:49:22 +0700

    [sprint-4][code] create docs api leeson student

--- src/main/java/com/hit/classservice/adapter/web/v1/controller/LessonStudentController.java
+++ src/main/java/com/hit/classservice/adapter/web/v1/controller/LessonStudentController.java
@@ -5,10 +5,16 @@ import com.hit.classservice.adapter.web.base.VsResponseUtil;
 import com.hit.classservice.adapter.web.v1.transfer.parameter.lesson_student.CreateLessonStudentParameter;
 import com.hit.classservice.adapter.web.v1.transfer.response.ResponseHeader;
 import com.hit.classservice.application.UseCaseBus;
+import com.hit.classservice.application.constant.CommonConstant;
 import com.hit.classservice.application.constant.UrlConstant;
 import com.hit.classservice.application.input.lesson_student.CreateLessonStudentInput;
 import com.hit.classservice.application.mapper.LessonStudentMapper;
 import com.hit.classservice.application.output.lesson_student.CreateLessonStudentOutput;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.mapstruct.factory.Mappers;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.ResponseEntity;
@@ -30,8 +36,21 @@ public class LessonStudentController {
     this.lessonStudentMapper = Mappers.getMapper(LessonStudentMapper.class);
   }
 
+  /**
+   * @param parameter CreateLessonStudentParameter
+   * @return ResponseEntity<?>
+   */
+  @Operation(summary = "API create new lesson student")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "Create success",
+          content = {@Content(mediaType = CommonConstant.APPLICATION_JSON_TYPE,
+              schema = @Schema(implementation = CreateLessonStudentOutput.class))
+          })
+  })
   @PostMapping(UrlConstant.LessonStudent.CREATE)
-  public ResponseEntity<?> createSubject(@Valid @RequestBody CreateLessonStudentParameter parameter) throws Exception {
+  public ResponseEntity<?> createSubject(
+      @io.swagger.v3.oas.annotations.parameters.RequestBody(description = "Body of request to create new lesson student")
+      @Valid @RequestBody CreateLessonStudentParameter parameter) throws Exception {
     //Create input
     CreateLessonStudentInput input = lessonStudentMapper.toCreateLessonStudentInput(parameter);
     //Get output
